f4d273ceb02f3f93a0b6556a95309cef1d4e4282
Faiz Mohammad <81904684+faizxmohammad@users.noreply.github.com>
2023-07-28 18:13:55 +0530

    added: gadgets.jsx and made it responsive

--- src/components/Gadgets.jsx
+++ src/components/Gadgets.jsx
@@ -14,44 +14,44 @@ function Gadgets() {
             </div>
             <div className="gadgets__cards">
                 {
-                    gadgetsData.map((items) =>{
-                        return items.sectionItems.map ( (item,index)=>(
+                    gadgetsData.map((items) =>(
+                        items.sectionItems.map ( (item,index)=>(
                             item.sectionItems.map((product)=>(  
-                <div className="card__wrapper">
-                    <div className="cards">
-                        {/* Like Icon */}
-                        <div className="favorite__icon">
-                            <img src={LikeIcon} alt="" />
-                        </div>
-                        {/* Product Image */}
-                        <div className="image">
-                            <img src={product.itemMainImage} alt="" />
-                        </div>
-                    </div>
-                     {/* Product name and price */}
-                    <div className="products" key={index}>
-                            <div className="product__title">
-                                <h2>{product.itemName}</h2>
-                                <p>₹{product.itemPrice}</p>
-                            </div>
-                            <div className="product__desc">
-                                <p>{product.itemDescription}</p>
-                            </div>
-                            {/* Add rating here.. */}
-                            <div className="product__rating">
-                                <p>Rating: {product.itemRating.reduce((accumulator , currentValue) => accumulator + currentValue , 0)}</p>
-                            </div >
-                            <div className="cart__button">
-                                <button>Add to Cart</button>
-                            </div>
-                        </div>
-                </div>
-                 ))
-                      ))
+                                <div className="card__wrapper">
+                                    <div className="cards">
+                                        {/* Like Icon */}
+                                        <div className="favorite__icon">
+                                            <img src={LikeIcon} alt="" />
+                                        </div>
+                                        {/* Product Image */}
+                                        <div className="image">
+                                            <img src={product.itemMainImage} alt="" />
+                                        </div>
+                                    </div>
+                                    {/* Product name and price */}
+                                    <div className="products" key={index}>
+                                            <div className="product__title">
+                                                <h2>{product.itemName}</h2>
+                                                <p>₹{product.itemPrice}</p>
+                                            </div>
+                                            <div className="product__desc">
+                                                <p>{product.itemDescription}</p>
+                                            </div>
+                                             {/* Add rating here.. */}
+                                            <div className="product__rating">
+                                                <p>Rating: {product.itemRating.reduce((accumulator , currentValue) => accumulator + currentValue , 0)}</p>
+                                            </div >
+                                            <div className="cart__button">
+                                                <button>Add to Cart</button>
+                                            </div>
+                                        </div>
+                                </div>
+                            ))
+                        ))
                 
-            })
+                    ))
 
-        }
+                }
 
                 {/* above here */}
             </div>
